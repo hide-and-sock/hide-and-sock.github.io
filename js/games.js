7d00475892e791809ac4f645278dff2ec86b2dc2
ambuc <bucklnd2@illinois.edu>
2015-10-18 02:41:10 -0500

    update v5 w/ danny, nice

---
+++ js/games.js
@@ -0,0 +1,18 @@
+games = [
+	{
+		'longname' : 'Stockholm',
+		'datestring' : '24 October 2015 • 2-4pm',
+		'registerlink' : '',
+		'tallylink' : '',
+		'rankingslink' : '',
+		'facebooklink' : ''
+	},
+	{
+		'longname' : 'Sydney',
+		'datestring' : '24 August 2015 • 2-4pm',
+		'registerlink' : '',
+		'tallylink' : '',
+		'rankingslink' : '/rankings.html',
+		'facebooklink' : ''
+	}
+];
---
+++ js/rankings.js
@@ -0,0 +1,118 @@
+window.onload = function() { init() };
+
+$('#go').click(function(){
+  youData($('input#secret').val().toUpperCase());
+})
+
+// var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE&output=html';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1OzMyZkpnDol7aeA_fpEbhTpgAEVVfQrGsBFyu29IWso/pubhtml';
+
+var dataObject = {};
+
+function init() {
+  Tabletop.init( { key: public_spreadsheet_url,
+                   callback: showInfo,
+                   simpleSheet: false } )
+}
+
+function sortByKey(array, key) {
+  return array.sort(function(a, b) {
+      var x = a[key]; var y = b[key];
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+
+function showInfo(data, tabletop) {
+  datafinds = data.finds;
+  console.log(data);
+
+  var codes = ['Code 1','Code 2','Code 3','Code 4','Code 5'];
+
+  //initialize dataObject
+  for (var i=0; i < datafinds.elements.length; i++){
+    // console.log(datafinds.elements[i]);
+    var name_phrase = data.finds.column_names[1];
+    var row = datafinds.elements[i]
+    var secret = row[name_phrase].toUpperCase();
+    // console.log(secret);
+    if (!(secret in dataObject)){
+      dataObject[secret] = {
+        'finds':[],
+        'founds':[],
+        'name':"",
+        'base':100,
+        'bonus':0
+      };
+    }
+
+    //initialize Finds
+    for (var j = 0; j < codes.length; j++){
+      if(row[codes[j]]!==""){
+        dataObject[secret].finds.push(row[codes[j]].toUpperCase());
+      }
+    }
+  }
+
+  //initialize Founds
+  for(el in dataObject){
+    for(var i = 0; i<dataObject[el].finds.length; i++){
+      if(dataObject[el].finds[i] in dataObject){
+        dataObject[dataObject[el].finds[i]].founds.push(el.toUpperCase());
+      }
+    }
+  }
+
+  //sweep NamesArray
+  for(var i=0; i < data.userData.elements.length; i++){
+    if (data.userData.elements[i]['Code'] in dataObject){
+      dataObject[data.userData.elements[i]['Code']].name = data.userData.elements[i]['Name'];
+    }
+  }
+
+  //tally points
+  for(el in dataObject){
+    dataObject[el].base=100.0/(dataObject[el].founds.length+1);
+    // console.log(dataObject[el].founds.length);
+    for(var i=0; i<dataObject[el].founds.length; i++){
+      dataObject[dataObject[el].founds[i]].bonus += dataObject[el].base;
+    }
+  }
+
+  //combine base, bonus to score
+  for(el in dataObject){
+    dataObject[el].score = dataObject[el].base + dataObject[el].bonus;
+  }
+
+  //create dataArray from dataObject
+  var dataArray = new Array;
+  for(var o in dataObject) {
+      dataArray.push(dataObject[o]);
+  }
+
+  //sort dataArray by score
+  dataArray.sort(function(a,b){return a.score < b.score;});
+
+  //print to table
+  for(var i=0; i<dataArray.length; i++){
+    $('#tops tbody').append("<tr> <td>"+dataArray[i].name+"</td> <td>"+parseInt(dataArray[i].score)+"</td> <td>"+dataArray[i].finds.length+"</td> <td>"+dataArray[i].founds.length+"</td> </tr>");
+  }
+
+  console.log(dataObject);
+}
+
+function youData(secret){
+  console.log(secret);
+  $('div#you_data').removeClass('hide', 1000, "easeInBack");
+  var obj = dataObject[secret];
+  for(var i = 0; i < obj.finds.length; i++){
+    $('ul#you_finds').append('<li>'+dataObject[obj.finds[i]].name+'</li>');
+
+  }
+  for(var i = 0; i < obj.founds.length; i++){
+    $('ul#you_founds').append('<li>'+dataObject[obj.founds[i]].name+'</li>');  
+  }
+}
+
+
+
+
