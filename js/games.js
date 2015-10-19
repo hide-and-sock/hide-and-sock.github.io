80bb8e74963837dd6a65d7dff527cbeac40db7d2
ambuc <bucklnd2@illinois.edu>
2015-10-19 10:53:57 -0500

    ok now it works lol

--- js/games.js
+++ js/games.js
@@ -1,4 +1,4 @@
-//hasOccured is -1 if before, 0 if during, 1 if after, 2 if too far after
+//hasOccured is -1 if before, 0 if during, 1 if after
 games = [
 	{
 		'longname' : 'Sydney',
@@ -8,14 +8,5 @@ games = [
 		'rankingslink' : 'rankings/sydney.html',
 		'facebooklink' : 'https://www.facebook.com/events/764367393671643/',
 		'hasOccurred' : -1
-	},
-	{
-		'longname' : 'Stockholm',
-		'datestring' : '16 May 2015 • 1:45-4:00pm UTC+02',
-		'registerlink' : '/404.html',
-		'tallylink' : '/404.html',
-		'rankingslink' : '/404.html',
-		'facebooklink' : 'https://www.facebook.com/events/1568336823455138/',
-		'hasOccurred' : 2
 	}
 ];
