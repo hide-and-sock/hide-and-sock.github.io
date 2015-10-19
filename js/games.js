483747b6f8bbfe0bc7fe06f5b0eb048135db97d0
ambuc <bucklnd2@illinois.edu>
2015-10-19 10:10:16 -0500

    sydney2

--- js/games.js
+++ js/games.js
@@ -3,8 +3,8 @@ games = [
 	{
 		'longname' : 'Sydney',
 		'datestring' : '12 December 2015 • 4:00pm UTC+11',
-		'registerlink' : 'https://docs.google.com/forms/d/1U0FoHlajmjlSEwVJF2RmbT3fX3nqDHF1P38zQoVrf3E/viewform',
-		'tallylink' : 'https://docs.google.com/forms/d/1vkZqjjwSJzysPsS-kp5dNRkYUu56Mh-uWr0VJWdpIxA/viewform',
+		'registerlink' : 'https://docs.google.com/forms/d/1uwfvrcUI7ReOU7q9qakTFsgSxoaP9r5UN6r1X15O8KE/viewform',
+		'tallylink' : 'https://docs.google.com/forms/d/1TtbdBXVoavjNVjAyqI0qQQqw5YdjAODIOW5P1rddxoA/viewform',
 		'rankingslink' : 'rankings/sydney.html',
 		'facebooklink' : 'https://www.facebook.com/events/764367393671643/',
 		'hasOccurred' : -1
