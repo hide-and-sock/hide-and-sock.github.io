81cfb4c1b07dd1a535d92b68d24db67111b082ac
ambuc <bucklnd2@illinois.edu>
2015-10-18 19:24:12 -0500

    live test

--- js/games.js
+++ js/games.js
@@ -1,12 +1,21 @@
-//hasOccured is -1 if before, 0 if during, 1 if after
+//hasOccured is -1 if before, 0 if during, 1 if after, 2 if too far after
 games = [
 	{
 		'longname' : 'Sydney',
-		'datestring' : '24 August 2015 • 2-4pm',
+		'datestring' : '12 December 2015 • 4:00pm UTC+11',
 		'registerlink' : 'URL',
 		'tallylink' : 'URL',
 		'rankingslink' : 'rankings/sydney.html',
-		'facebooklink' : 'URL',
+		'facebooklink' : 'https://www.facebook.com/events/764367393671643/',
 		'hasOccurred' : '-1'
+	},
+	{
+		'longname' : 'Stockholm',
+		'datestring' : '16 May 2015 • 1:45-4:00pm UTC+02',
+		'registerlink' : '/404.html',
+		'tallylink' : '/404.html',
+		'rankingslink' : '/404.html',
+		'facebooklink' : 'https://www.facebook.com/events/1568336823455138/',
+		'hasOccurred' : '2'
 	}
 ];
